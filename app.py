01b9b9ea8a78ffb011678e5a8dbb40480929b602
FG-SC <55146635+FG-SC@users.noreply.github.com>
2024-08-16 22:29:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def create_label_mapping(image_folder_path):
     return label_mapping
 
 # Load the trained model
-model = load_model('bird_species_classifier')
+model = load_model('bird_species_classifier.keras')
 #model = tf.keras.models.load_model("")
 
 # Path to the folder containing bird images organized in subfolders by species
